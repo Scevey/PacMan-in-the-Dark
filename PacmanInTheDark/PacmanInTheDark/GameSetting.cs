fa9a425e6fdef43afbb9c5e0ad6c6d50dd676c7f
Anthony Gialllella <ajg723@gmail.com>
2014-04-04 11:21:27 -0400

    Added class to read in the file

---
+++ PacmanInTheDark/PacmanInTheDark/GameSetting.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace PacmanInTheDark
+{
+    //Anthony Giallella
+    class GameSetting
+    {
+        //load in file from editor
+        public void Load()
+        {
+            // to store the data
+            List<int> data = new List<int>();
+
+            //parse text file to ints with this
+            int val;
+
+            StreamReader input = null;
+            try
+            {
+                //open stream
+                input = new StreamReader("../../../../../ToolForPacman.ToolForPacman.bin.Debug.Editor.txt");
+                string text = "";
+                //take text, parse, add to list
+                while ((text = input.ReadLine()) != null)
+                {
+                    val = int.Parse(text);
+                    data.Add(val);
+                }
+            }
+            //if no file add default values
+            catch (FileNotFoundException)
+            {
+                data.Add(3);
+                data.Add(5);
+                data.Add(3);
+                data.Add(100);
+                data.Add(1);
+                data.Add(3);
+            }
+
+        }
+    }
+}
