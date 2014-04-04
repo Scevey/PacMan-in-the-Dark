c56e02e6bb142f9036bfeea630dc4102829eef2a
Jeremy <jeremyisabeast@live.com>
2014-04-03 21:52:14 -0700

    Added tool editor!! Yeahh buddy

---
+++ ToolForPacman/ToolForPacman/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace ToolForPacman
+{
+    partial class ToolEditorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblWelcome = new System.Windows.Forms.Label();
+            this.txtSpeed = new System.Windows.Forms.TextBox();
+            this.lblLives = new System.Windows.Forms.Label();
+            this.lblSpeed = new System.Windows.Forms.Label();
+            this.lblGhosts = new System.Windows.Forms.Label();
+            this.lblLight = new System.Windows.Forms.Label();
+            this.txtGhosts = new System.Windows.Forms.TextBox();
+            this.lblHealth = new System.Windows.Forms.Label();
+            this.txtHealth = new System.Windows.Forms.TextBox();
+            this.txtLives = new System.Windows.Forms.TextBox();
+            this.lblHunger = new System.Windows.Forms.Label();
+            this.btnHelp = new System.Windows.Forms.Button();
+            this.txtHunger = new System.Windows.Forms.TextBox();
+            this.txtLight = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblWelcome
+            // 
+            this.lblWelcome.AutoSize = true;
+            this.lblWelcome.Font = new System.Drawing.Font("Impact", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWelcome.Location = new System.Drawing.Point(35, 9);
+            this.lblWelcome.Name = "lblWelcome";
+            this.lblWelcome.Size = new System.Drawing.Size(221, 90);
+            this.lblWelcome.TabIndex = 0;
+            this.lblWelcome.Text = "Pacman Tool \r\nEditor";
+            this.lblWelcome.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // txtSpeed
+            // 
+            this.txtSpeed.Location = new System.Drawing.Point(80, 113);
+            this.txtSpeed.Name = "txtSpeed";
+            this.txtSpeed.Size = new System.Drawing.Size(63, 20);
+            this.txtSpeed.TabIndex = 1;
+            // 
+            // lblLives
+            // 
+            this.lblLives.AutoSize = true;
+            this.lblLives.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLives.Location = new System.Drawing.Point(12, 147);
+            this.lblLives.Name = "lblLives";
+            this.lblLives.Size = new System.Drawing.Size(46, 20);
+            this.lblLives.TabIndex = 3;
+            this.lblLives.Text = "Lives:";
+            // 
+            // lblSpeed
+            // 
+            this.lblSpeed.AutoSize = true;
+            this.lblSpeed.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSpeed.Location = new System.Drawing.Point(12, 113);
+            this.lblSpeed.Name = "lblSpeed";
+            this.lblSpeed.Size = new System.Drawing.Size(52, 20);
+            this.lblSpeed.TabIndex = 4;
+            this.lblSpeed.Text = "Speed:";
+            // 
+            // lblGhosts
+            // 
+            this.lblGhosts.AutoSize = true;
+            this.lblGhosts.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGhosts.Location = new System.Drawing.Point(145, 113);
+            this.lblGhosts.Name = "lblGhosts";
+            this.lblGhosts.Size = new System.Drawing.Size(58, 20);
+            this.lblGhosts.TabIndex = 5;
+            this.lblGhosts.Text = "Ghosts:";
+            // 
+            // lblLight
+            // 
+            this.lblLight.AutoSize = true;
+            this.lblLight.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLight.Location = new System.Drawing.Point(12, 206);
+            this.lblLight.Name = "lblLight";
+            this.lblLight.Size = new System.Drawing.Size(43, 20);
+            this.lblLight.TabIndex = 6;
+            this.lblLight.Text = "Light:";
+            // 
+            // txtGhosts
+            // 
+            this.txtGhosts.Location = new System.Drawing.Point(209, 113);
+            this.txtGhosts.Name = "txtGhosts";
+            this.txtGhosts.Size = new System.Drawing.Size(63, 20);
+            this.txtGhosts.TabIndex = 7;
+            // 
+            // lblHealth
+            // 
+            this.lblHealth.AutoSize = true;
+            this.lblHealth.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHealth.Location = new System.Drawing.Point(145, 147);
+            this.lblHealth.Name = "lblHealth";
+            this.lblHealth.Size = new System.Drawing.Size(54, 20);
+            this.lblHealth.TabIndex = 8;
+            this.lblHealth.Text = "Health:";
+            // 
+            // txtHealth
+            // 
+            this.txtHealth.Location = new System.Drawing.Point(209, 147);
+            this.txtHealth.Name = "txtHealth";
+            this.txtHealth.Size = new System.Drawing.Size(63, 20);
+            this.txtHealth.TabIndex = 9;
+            // 
+            // txtLives
+            // 
+            this.txtLives.Location = new System.Drawing.Point(80, 147);
+            this.txtLives.Name = "txtLives";
+            this.txtLives.Size = new System.Drawing.Size(63, 20);
+            this.txtLives.TabIndex = 10;
+            // 
+            // lblHunger
+            // 
+            this.lblHunger.AutoSize = true;
+            this.lblHunger.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHunger.Location = new System.Drawing.Point(12, 177);
+            this.lblHunger.Name = "lblHunger";
+            this.lblHunger.Size = new System.Drawing.Size(59, 20);
+            this.lblHunger.TabIndex = 11;
+            this.lblHunger.Text = "Hunger:";
+            // 
+            // btnHelp
+            // 
+            this.btnHelp.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHelp.Location = new System.Drawing.Point(153, 177);
+            this.btnHelp.Name = "btnHelp";
+            this.btnHelp.Size = new System.Drawing.Size(120, 32);
+            this.btnHelp.TabIndex = 12;
+            this.btnHelp.Text = "HELP";
+            this.btnHelp.UseVisualStyleBackColor = true;
+            this.btnHelp.Click += new System.EventHandler(this.btnHelp_Click);
+            // 
+            // txtHunger
+            // 
+            this.txtHunger.Location = new System.Drawing.Point(80, 177);
+            this.txtHunger.Name = "txtHunger";
+            this.txtHunger.Size = new System.Drawing.Size(63, 20);
+            this.txtHunger.TabIndex = 13;
+            // 
+            // txtLight
+            // 
+            this.txtLight.Location = new System.Drawing.Point(80, 206);
+            this.txtLight.Name = "txtLight";
+            this.txtLight.Size = new System.Drawing.Size(63, 20);
+            this.txtLight.TabIndex = 14;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Location = new System.Drawing.Point(153, 215);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(54, 32);
+            this.btnSave.TabIndex = 15;
+            this.btnSave.Text = "SAVE";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.Location = new System.Drawing.Point(213, 215);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(60, 32);
+            this.btnClose.TabIndex = 16;
+            this.btnClose.Text = "CLOSE";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // ToolEditorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtLight);
+            this.Controls.Add(this.txtHunger);
+            this.Controls.Add(this.btnHelp);
+            this.Controls.Add(this.lblHunger);
+            this.Controls.Add(this.txtLives);
+            this.Controls.Add(this.txtHealth);
+            this.Controls.Add(this.lblHealth);
+            this.Controls.Add(this.txtGhosts);
+            this.Controls.Add(this.lblLight);
+            this.Controls.Add(this.lblGhosts);
+            this.Controls.Add(this.lblSpeed);
+            this.Controls.Add(this.lblLives);
+            this.Controls.Add(this.txtSpeed);
+            this.Controls.Add(this.lblWelcome);
+            this.Name = "ToolEditorForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblWelcome;
+        private System.Windows.Forms.TextBox txtSpeed;
+        private System.Windows.Forms.Label lblLives;
+        private System.Windows.Forms.Label lblSpeed;
+        private System.Windows.Forms.Label lblGhosts;
+        private System.Windows.Forms.Label lblLight;
+        private System.Windows.Forms.TextBox txtGhosts;
+        private System.Windows.Forms.Label lblHealth;
+        private System.Windows.Forms.TextBox txtHealth;
+        private System.Windows.Forms.TextBox txtLives;
+        private System.Windows.Forms.Label lblHunger;
+        private System.Windows.Forms.Button btnHelp;
+        private System.Windows.Forms.TextBox txtHunger;
+        private System.Windows.Forms.TextBox txtLight;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
+
---
+++ ToolForPacman/ToolForPacman/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ToolForPacman
+{
+    public partial class ToolEditorForm : Form
+    {
+        //Made by Jeremy
+
+        public ToolEditorForm()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Provides information about each attribute
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnHelp_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Speed: How fast Pacman travels (1-10).\n" +
+                            "Ghosts: How many ghosts are there (1-5).\n" +
+                            "Lives: How many lives you start with (1-5).\n" +
+                            "Health: How much health you start with (25-200).\n" +
+                            "Hunger: How fast your health drains(1-5).\n" +
+                            "Light: The level of light around you(1-3).\n");
+        }
+
+        //Was the save successful?
+        bool saveSuccessful = false;
+
+        //Name of the saved file
+        string saveFile = "Editor.txt";
+
+        //attributes
+        int speed;
+        int ghosts;
+        int lives;
+        int health;
+        int hunger;
+        int light;
+
+        //Stream to add info to the save file
+        StreamWriter input;
+
+        /// <summary>
+        /// User saves data
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Create the stream
+                input = new StreamWriter(saveFile);
+
+                //Set the attributes
+                speed = int.Parse(txtSpeed.Text);
+                ghosts = int.Parse(txtGhosts.Text);
+                lives = int.Parse(txtLives.Text);
+                health = int.Parse(txtHealth.Text);
+                hunger = int.Parse(txtHunger.Text);
+                light = int.Parse(txtLight.Text);
+
+                //check for incorrect values
+                if (speed < 1 || speed > 11 || ghosts < 1 || ghosts > 5
+                    || lives < 1 || lives > 5 || health < 25 || health > 200
+                    || hunger < 1 || hunger > 5 || light < 1 || light > 3)
+                    saveSuccessful = false;
+                else
+                {
+                    //no incorrect values, OK to proceed
+                    saveSuccessful = true;
+
+                    //Populate the text file with user's entered attributes
+                    input.WriteLine(speed);
+                    input.WriteLine(ghosts);
+                    input.WriteLine(lives);
+                    input.WriteLine(health);
+                    input.WriteLine(hunger);
+                    input.WriteLine(light);
+                }
+                //Close the stream
+                input.Close();
+            }
+            catch (IOException ioe)
+            {
+                saveSuccessful = false;
+                MessageBox.Show("Error: " + ioe.Message);
+            }
+            catch (Exception)
+            {
+                saveSuccessful = false;
+                MessageBox.Show("Error saving file. Check for errors (Ie. invalid characters).");
+            }
+
+            //Alert user how to fix a possible save malfunction cause
+            if (saveSuccessful == false)
+                MessageBox.Show("Save was unsuccessful. See help box to view allowed values.");
+        }
+
+        /// <summary>
+        /// User wants to close editor
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("The Pacman editor will now close.");
+            this.Close();
+        }
+    }
+}
