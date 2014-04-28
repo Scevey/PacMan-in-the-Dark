0ed7d146f30b975fa0480f2fa21dc6b186b27498
Mike Teixeira <miketeixeira95@gmail.com>
2014-04-28 17:17:53 -0400

    Pellets are active when created and rendered inactive on collision with pacman. They won't draw or execute collision code when inactive.

--- PacmanInTheDark/PacmanInTheDark/Pellet.cs
+++ PacmanInTheDark/PacmanInTheDark/Pellet.cs
@@ -18,22 +18,29 @@ namespace PacmanInTheDark
         public Pellet(Path p, float pos)
             : base(p,pos)
         {
-
+            this.active = true;
         }
         public override void Draw(Microsoft.Xna.Framework.GameTime gameTime, Microsoft.Xna.Framework.Graphics.SpriteBatch spriteBatch, Point topLeft, Point bottomRight)
         {
-            throw new NotImplementedException();
+            //leave this line in
+            if (!active) return;
+
+            //add code here
         }
 
         public override void Collision(Pacman pac)
         {
+            //leave this line
+            if (!active) return;
+
             // collides with pacman - becomes inactive
             this.active = false;
+
+            //add code here
         }
 
         public override void Collision(Ghost ghost)
         {
-            throw new NotImplementedException();
         }
     }
 }
